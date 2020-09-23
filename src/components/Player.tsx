4bc8b250f78d5fcd02541e58ed7a9e433d3c6ce9
senbonsakura <overlord_cihat@yahoo.com>
2020-09-23 15:50:32 +0300

    Subtitle styling

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -18,18 +18,19 @@ const Player = ({file, subtitle, name, time, onFinish}: PlayerProps) => {
 
     const onEnded = () => {
         setEnded(true)
-
-
     }
-    const videoRef: any = useRef<HTMLVideoElement>(null)
-    const trackRef: any = useRef<HTMLTrackElement>(null)
+
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const trackRef = useRef<HTMLTrackElement>(null)
     const saveFile = useCallback(() => {
         window.localStorage.setItem('currentFile', file)
         window.localStorage.setItem('currentSubtitle', subtitle)
     }, [file, subtitle])
 
     const saveTime = () => {
-        window.localStorage.setItem('currentTime', videoRef.current.currentTime)
+        if (videoRef && videoRef.current) {
+            window.localStorage.setItem('currentTime', videoRef.current.currentTime.toString())
+        }
     }
 
     useEffect(() => {
@@ -39,6 +40,10 @@ const Player = ({file, subtitle, name, time, onFinish}: PlayerProps) => {
         }
     },)
 
+    interface TextTrackCueWithLine extends TextTrackCue {
+        line: number
+    }
+
     useEffect(() => {
 
         if (subtitle.endsWith("srt")) {
@@ -47,12 +52,34 @@ const Player = ({file, subtitle, name, time, onFinish}: PlayerProps) => {
                 .then(blob => {
                     const vttConverter = new VTTConverter(blob)
                     vttConverter.getURL().then((url: string) => {
-                        trackRef.current.src = url
+                        if (trackRef && trackRef.current) {
+                            trackRef.current.src = url
+
+                        }
                         //videoRef.current.textTracks[0].mode = "show"
                     })
                 })
+        }
+        const onTrackLoaded = () => {
+            console.log("track loaded")
+            if (trackRef && trackRef.current) {
+                const track = trackRef.current.track
+                const cues = track.cues
+                for (let j = 0;j < cues.length;j++) {
+                    console.log(cues[j])
+                    let new_cue = new VTTCue(cues[j].startTime,cues[j].endTime, cues[j].text);
+                    new_cue.line = 98
+                    new_cue.snapToLines = false
+                    track.removeCue(cues[j])
+                    track.addCue(new_cue)
 
+                }
+                console.log("is ready?",track)
 
+            }
+        }
+        if (trackRef && trackRef.current) {
+            trackRef.current.addEventListener("load", onTrackLoaded)
         }
         //file && videoRef.current.play()
         file && saveFile()
@@ -62,12 +89,14 @@ const Player = ({file, subtitle, name, time, onFinish}: PlayerProps) => {
     return (
         <div>
             {isEnded && <PlayNext setEnded={setEnded} onFinish={onFinish}/>}
-            <video id="video" controls preload="metadata" ref={videoRef} key={file} onEnded={onEnded} autoPlay width={'100%'}>
+            <video id="video" controls preload="metadata" ref={videoRef} key={file} onEnded={onEnded} autoPlay
+                   width={'100%'}>
                 <source src={`${file}${time > 0 ? `#t=${time}` : ''}`} type="video/mp4"/>
-                <track ref={trackRef} label="English" kind="subtitles" srcLang="en" src={subtitle.endsWith("srt") ? undefined : subtitle} default/>
+                <track ref={trackRef} label="English" kind="subtitles" srcLang="en"
+                       src={subtitle.endsWith("srt") ? undefined : subtitle} default/>
             </video>
         </div>
-);
+    );
 };
 
 export default Player;
